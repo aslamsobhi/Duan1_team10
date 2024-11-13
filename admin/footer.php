bdd9807e6f781a5d1bd6cf911dbc4b9ce29283a8
MinhDuy <duyhvmph33009@fpt.edu.vn>
2024-11-13 23:31:09 +0700

    Initial commit

---
+++ admin/footer.php
@@ -0,0 +1,18 @@
+<!-- Footer -->
+<footer class="main-footer">
+  <strong>Copyright &copy; 2014-2021 <a href="#">Your Company</a>.</strong>
+  All rights reserved.
+  <div class="float-right d-none d-sm-inline-block">
+    <b>Version</b> 3.2.0
+  </div>
+</footer>
+
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+</aside>
+<!-- Scripts -->
+<script src="/Duan1_nhom10/adminlte/plugins/jquery/jquery.min.js"></script>
+<script src="/Duan1_nhom10/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="/Duan1_nhom10/adminlte/dist/js/adminlte.min.js"></script>
+</body>
+</html>
